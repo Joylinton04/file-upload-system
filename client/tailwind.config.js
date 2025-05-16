9fbd689f9b8001eb293d5cbe76d01f88f9f3a849
Joylinton04 <adujoy05@gmail.com>
2025-05-16 04:04:22 +0000

    tailwind and font setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        default: "Poppins, sans-serif",
+      }
+    },
   },
   plugins: [],
 }
