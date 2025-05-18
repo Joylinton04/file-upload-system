614540ee197b1a46a1a89754ace9c56708dd894e
Joylinton04 <adujoy05@gmail.com>
2025-05-18 23:25:40 +0000

    status from local storage

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,7 +1,9 @@
 import React, { createContext, useEffect, useState, type ReactNode } from "react";
 
+type Status = "local" | "uploaded";
+
 interface FileMeta {
-  id: string,
+  id: string;
   name: string;
   size: number;
   type: string;
@@ -11,6 +13,8 @@ interface FileMeta {
 interface AppContextValue {
   file: FileMeta[];
   setFile: React.Dispatch<React.SetStateAction<FileMeta[]>>;
+  handleStatus: Status;
+  setHandleStatus: React.Dispatch<React.SetStateAction<Status>>;
 }
 
 interface AppContextProviderProps {
@@ -21,15 +25,31 @@ export const AppContent = createContext<AppContextValue | null>(null);
 
 export const AppContextProvider = ({ children }: AppContextProviderProps) => {
   const fileData = localStorage.getItem("files");
+  const status = localStorage.getItem("Status");
+
   const savedFiles: FileMeta[] = fileData ? JSON.parse(fileData) : [];
 
+  let currentStatus: Status = "local";
+  try {
+    if (status && status !== "undefined") {
+      currentStatus = JSON.parse(status);
+    }
+  } catch (err) {
+    console.warn("Invalid status in localStorage:", err);
+  }
+
   const [file, setFile] = useState<FileMeta[]>(savedFiles);
+  const [handleStatus, setHandleStatus] = useState<Status>(currentStatus);
 
   useEffect(() => {
     localStorage.setItem("files", JSON.stringify(file));
   }, [file]);
 
-  const value = { file, setFile };
+  useEffect(() => {
+    localStorage.setItem("Status", JSON.stringify(handleStatus));
+  }, [handleStatus]);
+
+  const value = { file, setFile, handleStatus, setHandleStatus };
 
   return <AppContent.Provider value={value}>{children}</AppContent.Provider>;
 };
